e1be668859bbb16dc96711670eea1318937eb3e6
Darío Rodríguez García <dario.rg.inf@gmail.com>
2016-04-26 16:11:38 +0200

    Modificado subsistema VotingSystem en vistas y lógica

--- VotingSystem_3a/Procfile
+++ VotingSystem_3a/Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/VotingSystem_3a-0.0.1.jar
+web: java -Dserver.port=$PORT -jar target/VotingSystem_3a-0.0.1.jar
--- VotingSystem_3a/src/main/java/es/uniovi/asw/persistence/dbManagement/votingDBManagement/impl/GetActiveVotingsImpl.java
+++ VotingSystem_3a/src/main/java/es/uniovi/asw/persistence/dbManagement/votingDBManagement/impl/GetActiveVotingsImpl.java
@@ -1,11 +1,13 @@
 package es.uniovi.asw.persistence.dbManagement.votingDBManagement.impl;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import es.uniovi.asw.model.Eleccion;
 import es.uniovi.asw.persistence.dbManagement.repository.VotingRepository;
-
-import java.util.ArrayList;
-
 import es.uniovi.asw.persistence.dbManagement.votingDBManagement.GetActiveVotings;
 
 class GetActiveVotingsImpl implements GetActiveVotings {
@@ -13,18 +15,35 @@ class GetActiveVotingsImpl implements GetActiveVotings {
 	@Override
 	public List<Eleccion> getActiveVotings(VotingRepository vr) {
 		List<Eleccion> resultado = new ArrayList<Eleccion>();
-		for(Eleccion e : vr.findAll()){
-			if(e.isActiva())
+		for (Eleccion e : vr.findAll()) {
+			if (e.isActiva()) {
 				resultado.add(e);
+			}
 		}
 		return resultado;
 	}
-	
-	public List<Eleccion> eleccionesActivas(VotingRepository vRep){
-		return vRep.findByActivaTrue();
+
+	public List<Eleccion> eleccionesActivas(VotingRepository vRep) {
+		List<Eleccion> elecciones = vRep.findByActivaTrue();
+		List<Eleccion> aux = new ArrayList<Eleccion>();
+		Date inicio, fin = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for(Eleccion e : elecciones){
+			try {
+				inicio = sdf.parse(e.getFechaInicio().toString());
+				fin = sdf.parse(e.getFechaFin().toString());
+				if(inicio.getTime() < new Date(System.currentTimeMillis()).getTime() 
+						&& fin.getTime() > new Date(System.currentTimeMillis()).getTime()){
+					aux.add(e);
+				}
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+		}
+		return aux;
 	}
-	
-	public Eleccion buscarPorId(VotingRepository vRep, Long idEleccion){
+
+	public Eleccion buscarPorId(VotingRepository vRep, Long idEleccion) {
 		return vRep.findOne(idEleccion);
 	}
 
